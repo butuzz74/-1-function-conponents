e5ffb0b5bc2a616a2a70ba1ee965ccf578ff9a3b
Alexey Danilchik <butuzz74@gmail.com>
2023-03-30 13:55:53 +0300

    deactive item GroupList

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -15,6 +15,7 @@ const Users = (props) => {
     const [activePage, setActivePage] = useState(1);
     const [professions, setProfessions] = useState();
     const [selectedProf, setSelectedProf] = useState();
+    const [deactiveProf, setDeactiveProf] = useState();
 
     useEffect(() => {
         API.professions.fetchAll().then((data) => setProfessions(data));
@@ -26,7 +27,9 @@ const Users = (props) => {
 
     const handleSelectedProf = (item) => {
         setSelectedProf(item);
+        setDeactiveProf(item.name);
     };
+    console.log(deactiveProf);
     const handleClearSelected = () => {
         setSelectedProf();
     };
